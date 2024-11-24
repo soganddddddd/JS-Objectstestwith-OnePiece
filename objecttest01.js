0fbb5f17e3b8a0879a7a2dd1767947a6c7c16077
sowgand <ramezanzadesogand@gmail.com>
2024-11-24 16:57:11 +0330

    Update objecttest01.js

--- objecttest01.js
+++ objecttest01.js
@@ -1,7 +1,47 @@
 const captain = {
     Name: "Luffy",
     LastName: "D.Monkey",
-    Age:16,
-    title:"strawhat",
-    
+    Age:19,
+    title:"straw hat",
+    job:"caption",
+}
+
+const crew_1 = {
+    Name: "Zoro",
+    LastName: "Rononoa",
+    Age:21,
+    title:"Pirate Hunter",
+    job:"3sword-swordman"
+}
+
+const crew_2 = {
+    Name: "Nami",
+    LastName: "None",
+    Age:20,
+    title:"Cat Burglar",
+    job:"Navigator"
+}
+
+const crew_3 = {
+    Name: "Ussop",
+    LastName: "None",
+    Age:19,
+    title:"GodUssop",
+    job:"shooter - inventor"
+}
+
+const crew_4 = {
+    Name: "Chopper",
+    LastName: "Tony Tony",
+    Age:17,
+    title:"IDK;(",
+    job:"Doctor"
+}
+
+const crew_5 = {
+    Name: "Nami",
+    LastName: "None",
+    Age:20,
+    title:"Cat Burglar",
+    job:"Navigator"
 }
